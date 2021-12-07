73e6bb8387c5d04da1043c1aea07fa203e335185
iamRomeo-dev <romeo.brilland@live.fr>
2021-12-07 14:17:29 +0100

    perf: :alien: Add useId params on the API calls

--- src/API.js
+++ src/API.js
@@ -1,14 +1,27 @@
-const url = "http://localhost:3000/user/12/performance";
-const url2 = "http://localhost:3000/user/12";
-
-export const fetchPerformance = async () => {
-  const res = await fetch(url);
+export const fetchUser = async (userId) => {
+  const urlUser = `http://localhost:3000/user/${userId}`;
+  const res = await fetch(urlUser);
   const json = await res.json();
   return json;
 };
 
-export const fetchUser = async () => {
-  const res = await fetch(url2);
+export const fetchPerformance = async (userId) => {
+  const urlPerf = `http://localhost:3000/user/${userId}/performance`;
+  const res = await fetch(urlPerf);
+  const json = await res.json();
+  return json;
+};
+
+export const fetchActivity = async (userId) => {
+  const urlActi = `http://localhost:3000/user/${userId}/activity`;
+  const res = await fetch(urlActi);
+  const json = await res.json();
+  return json;
+};
+
+export const fetchAverageSession = async (userId) => {
+  const urlAverSesh = `http://localhost:3000/user/${userId}/average-sessions`;
+  const res = await fetch(urlAverSesh);
   const json = await res.json();
   return json;
 };
