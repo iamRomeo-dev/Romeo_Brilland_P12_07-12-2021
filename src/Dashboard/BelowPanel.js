c026c3834d336195144bd1e93e1e345bc7d6f674
iamRomeo-dev <romeo.brilland@live.fr>
2021-12-07 20:01:05 +0100

    feat: :sparkles: Add line chart on below panel

--- src/Dashboard/BelowPanel.js
+++ src/Dashboard/BelowPanel.js
@@ -1,9 +1,13 @@
+import { ThelineChart } from "../Charts/TheLineChart";
+
 export const BelowPanel = () => {
   return (
-    <div className="flex flex-row gap-2">
-      <div className="w-full h-24 bg-gray-100"></div>
-      <div className="w-full h-24 bg-gray-100"></div>
-      <div className="w-full h-24 bg-gray-100"></div>
+    <div className="flex gap-2">
+      <div className="w-full h-64 bg-red-500">
+        <ThelineChart />
+      </div>
+      <div className="w-full bg-gray-100"></div>
+      <div className="w-full bg-gray-100"></div>
     </div>
   );
 };
