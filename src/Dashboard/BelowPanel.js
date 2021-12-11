3ea3e97302ed3c2a853d61829c51339a6dc6f252
iamRomeo-dev <romeo.brilland@live.fr>
2021-12-11 19:22:39 +0100

    feat: :sparkles: Add score panel on the below panel

--- src/Dashboard/BelowPanel.js
+++ src/Dashboard/BelowPanel.js
@@ -1,7 +1,13 @@
+import { useQuery } from "react-query";
+import { fetchUser } from "../API";
 import { ThelineChart } from "../Charts/TheLineChart";
 import { TheRadarChart } from "../Charts/TheRadarChart";
+import { useUser } from "../MyContext";
 
 export const BelowPanel = () => {
+  const user = useUser();
+  const { data, status } = useQuery(["product", user], () => fetchUser(user));
+  console.log(data?.data.todayScore);
   return (
     <div className="flex gap-2">
       <div className="w-full bg-red-500">
@@ -10,7 +16,56 @@ export const BelowPanel = () => {
       <div className="w-full bg-gray-900">
         <TheRadarChart />
       </div>
-      <div className="w-full bg-gray-100"></div>
+      <div className="w-full bg-gray-100 flex items-center justify-center">
+        {status === "loading" && (
+          <div className="ring-gray-200 text-primary-200 flex-shrink-0 h-16 w-16 flex items-center justify-center ring rounded-full overflow-hidden">
+            <span className="bg-gray-200 w-4 h-4 rounded-full"></span>
+          </div>
+        )}
+        {status === "error" && (
+          <div className="ring-gray-200 text-primary-200 flex-shrink-0 h-16 w-16 flex items-center justify-center ring rounded-full overflow-hidden">
+            <span className="bg-gray-200 w-4 h-4 rounded-full">Error</span>
+          </div>
+        )}
+        {status === "success" && (
+          <JobAdMatchingScore score={data.data.todayScore * 100} />
+        )}
+      </div>
     </div>
+  );
+};
+
+export const JobAdMatchingScore = ({ score }) => {
+  const displayedScore = score || 0;
+  return (
+    <>
+      {displayedScore >= 50 && (
+        <div className="relative bg-white flex-shrink-0 h-16 w-16 flex items-center justify-center ring ring-red-500 rounded-full overflow-hidden">
+          <span className="text-gray-900 text-sm font-semibold leading-none">
+            {score} %
+          </span>
+          <span className="absolute top-2/3 left-1/2 transform -translate-x-1/2 text-gray-400 text-xs text-center w-full">
+            de votre objectif
+          </span>
+        </div>
+      )}
+
+      {score ? (
+        displayedScore < 50 && (
+          <div className="relative bg-white flex-shrink-0 w-20 h-20 flex items-center justify-center border-l-4 border-red-500 rounded-full overflow-hidden">
+            <span className="text-gray-900 text-sm font-semibold leading-none">
+              {score} %
+            </span>
+            <span className="absolute top-2/3 left-1/2 transform -translate-x-1/2 text-gray-400 text-xs text-center w-full">
+              de votre objectif
+            </span>
+          </div>
+        )
+      ) : (
+        <div className="ring-gray-200 text-primary-200 flex-shrink-0 h-16 w-16 flex items-center justify-center ring rounded-full overflow-hidden">
+          <span className="bg-gray-200 w-4 h-4 rounded-full"></span>
+        </div>
+      )}
+    </>
   );
 };
