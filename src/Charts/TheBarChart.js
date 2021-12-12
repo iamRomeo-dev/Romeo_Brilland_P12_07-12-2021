6f913cb7c9e3bb396e8e9846cbf00b4b17586656
iamRomeo-dev <romeo.brilland@live.fr>
2021-12-12 09:30:49 +0100

    feat: :sparkles: Add good xAxis lengend

--- src/Charts/TheBarChart.js
+++ src/Charts/TheBarChart.js
@@ -18,6 +18,17 @@ export const TheBarChart = () => {
   const { data, status } = useQuery(["activity", user], () =>
     fetchActivity(user)
   );
+  console.log(data?.data.sessions[1].day.substr(-1));
+
+  const dataTransormed = data?.data.sessions;
+  const kindTransformed = data;
+  console.log("dataTransormed", dataTransormed);
+  console.log("kindTransformed", kindTransformed);
+
+  // Loop on day and keep only the last caracter of the string day. Then i say that dataTransormed[i].day will be now dataTransormed[i].day.substr(-1)
+  for (let i = 0; i < dataTransormed?.length; i++) {
+    dataTransormed[i].day = dataTransormed[i].day.substr(-1);
+  }
 
   return (
     <>
@@ -37,8 +48,13 @@ export const TheBarChart = () => {
             barGap={8}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis datakey="day" dy={15} />
-            <YAxis orientation="right" tick={{ fontSize: 14 }} />
+            <XAxis dataKey="day" dy={15} tickLine={false} />
+            <YAxis
+              // type="number"
+              // dataKey="kilogram"
+              orientation="right"
+              tick={{ fontSize: 14 }}
+            />
 
             <Tooltip />
             <Bar dataKey="kilogram" fill="#000" radius={[10, 10, 0, 0]} />
