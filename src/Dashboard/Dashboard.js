4404a419f48d74d60c8fb396b498ce332105ce2a
iamRomeo-dev <romeo.brilland@live.fr>
2021-12-13 11:34:53 +0100

    feat: :sparkles: Make margin top between welcome and panel

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -8,7 +8,7 @@ export const Dashboard = () => {
     <div className="flex-grow p-16">
       <Welcome />
       {/* Blocks */}
-      <div className="flex flex-wrap gap-8">
+      <div className="flex flex-wrap gap-8 mt-8">
         {/* Block de gauche */}
         <div className="flex flex-col flex-wrap gap-2 h-full min-w-800 lg:w-4/6">
           <AbovePanel />
